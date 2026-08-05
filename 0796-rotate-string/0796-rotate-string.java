bc9c93df204bd0deaa9140c6d7ee11e7ce71367f
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-05 20:06:45 +0530

    Time: 2 ms (50.25%), Space: 43.6 MB (12.18%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+       String a = s+s;
+       if(a.contains(goal)){
+        return true;
+       }
+       return false;
+    }
+}
