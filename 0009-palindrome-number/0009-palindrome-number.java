e37c850465dd67e63cf3cfbc547a8c348867c8ca
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-07 22:03:04 +0530

    Time: 5 ms (81.26%), Space: 46.1 MB (33.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        String s = String.valueOf(x);
-        char c[] = s.toCharArray();
+      int tp = 0;
+      int temp = x;
 
-        int i = 0, j = c.length - 1;
-
-        while(i <= j){
-            if(c[i] != c[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
+      while(temp > 0){
+        int a = temp%10;
+        tp = tp*10 + a;
+        temp = temp/10;
+      }
+      return tp == x;
     }
 }
