53801a173008784161db9091d3dd9db2048f9602
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-11 11:02:19 +0530

    Time: 4 ms (45.61%), Space: 45.3 MB (36.3%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        // int i = 0, j = s.length()- 1;
+        // s = s.toLowerCase();
+        // while(i < j){
+        //     if(!Character.isLetterOrDigit(s.charAt(i))) i++;
+        //     else if(!Character.isLetterOrDigit(s.charAt(j))) j--;
+            
+        //     else{
+        //         if(s.charAt(i) != s.charAt(j)){
+        //         return false;
+        //     }
+        //     i++;
+        //     j--;
+        //     }
+        // }
+        // return true;
+
+        int i = 0, j = s.length() - 1;
+        s = s.toLowerCase();
+        StringBuilder sb  = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)) sb.append(ch);
+        }
+        if(sb.toString().equals(sb.reverse().toString())) return true;
+        return false;
+    }
+}
