28b49be3ec2fe03cfd92fa7be46aa0acccfa1581
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-11 06:19:45 +0530

    Time: 2 ms (99.36%), Space: 122.6 MB (40.02%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -5,22 +5,11 @@ class Solution {
    
         int min1 = Integer.MAX_VALUE;
         int min2 = Integer.MAX_VALUE;
-        int min3 = Integer.MAX_VALUE;
-
+        
         for(int i = 0; i < n; i++){
-            if(nums[i] < min1){
-                min1 = nums[i];
-                min2 = min2;
-                min3 = min2;
-            }
-            else if(nums[i] > min1 && nums[i] < min2){
-                min2 = nums[i];
-                min3 = min2;
-            }
-            else if(nums[i] > min1 && nums[i] > min2 ){
-                min3 = nums[i];
-            }
-            if(min1 < min2 && min2 < min3) return true;
+            if(nums[i] <= min1) min1 = nums[i];
+            else if(nums[i] <= min2) min2 = nums[i];
+            else return true;
         }
         return false;
     }
