2f45e2645545c533c3de04b216d363b6c7b76f2b
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-12 09:14:54 +0530

    Time: 3 ms (87.36%), Space: 65.2 MB (93.5%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,18 +1,34 @@
 class Solution {
     public int longestSubarray(int[] nums) {
 
-        int ans = 0;
-        for(int i = 0; i < nums.length; i++){
-            int c0 = 0, c1 = 0;
-            for(int j = i; j < nums.length; j++){
-                if(nums[j] == 0) c0++;
-                if(nums[j] == 1) c1++;
+        //int ans = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     int c0 = 0, c1 = 0;
+        //     for(int j = i; j < nums.length; j++){
+        //         if(nums[j] == 0) c0++;
+        //         if(nums[j] == 1) c1++;
 
-                if(c0 > 1) break;
+        //         if(c0 > 1) break;
+        //     }
+        //     if(c0 == 0) c1--;
+        //      ans = Math.max(c1, ans);
+        // }
+
+        int i = 0, j = 0;
+        int c0 = 0, ans = 0, max = 0;
+        while(j < nums.length){
+            if(nums[j] == 0) c0++;
+            else if(nums[j] == 1) ans++;
+
+            while(c0 > 1){
+                ans = ans - nums[i];
+                if(nums[i] == 0) c0--;
+                i++;
             }
-            if(c0 == 0) c1--;
-             ans = Math.max(c1, ans);
+            max = Math.max(max, ans);
+            j++;
         }
-        return ans;
+        if(c0 == 0) max = max-1;
+        return max;
     }
 }
