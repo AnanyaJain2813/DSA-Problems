9941d7040bb264976eb3b58a29571cfbc3ca4c9e
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-18 12:02:45 +0530

    Time: 1 ms (99.96%), Space: 46.2 MB (79.72%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,22 +2,38 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         if(ransomNote.length() > magazine.length()) return false;
 
-        Map<Character, Integer> have = new HashMap<>();
-        Map<Character, Integer> need = new HashMap<>();
+        // Map<Character, Integer> have = new HashMap<>();
+        // Map<Character, Integer> need = new HashMap<>();
 
-        for(int i = 0; i < ransomNote.length(); i++){
+        // for(int i = 0; i < ransomNote.length(); i++){
+        //     char ch = ransomNote.charAt(i);
+        //     need.put(ch, need.getOrDefault(ch, 0)+1);
+        // }
+        // for(int i = 0; i < magazine.length(); i++){
+        //     char ch = magazine.charAt(i);
+        //     have.put(ch, have.getOrDefault(ch, 0)+1);
+        // }
+
+        // for(char ch : need.keySet()){
+        //     int fn = need.get(ch);
+        //     int fh = have.getOrDefault(ch,0);
+        //     if(fh < fn) return false;
+        // }
+
+        int n = ransomNote.length();
+        int m = magazine.length();
+
+        int []c = new int[26];
+        for(int i = 0; i < n; i++){
             char ch = ransomNote.charAt(i);
-            need.put(ch, need.getOrDefault(ch, 0)+1);
+            c[ch-'a']--;
         }
-        for(int i = 0; i < magazine.length(); i++){
-            char ch = magazine.charAt(i);
-            have.put(ch, have.getOrDefault(ch, 0)+1);
+        for(int j = 0; j < m; j++){
+            char ch = magazine.charAt(j);
+            c[ch - 'a']++;
         }
-
-        for(char ch : need.keySet()){
-            int fn = need.get(ch);
-            int fh = have.getOrDefault(ch,0);
-            if(fh < fn) return false;
+        for(int i = 0; i < c.length; i++){
+            if(c[i] < 0) return false;
         }
         return true;
     }
