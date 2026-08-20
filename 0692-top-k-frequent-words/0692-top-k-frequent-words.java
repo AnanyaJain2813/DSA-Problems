dcfebd9cd398ff4b9123e64d3a935debb0fe87b8
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-20 15:05:39 +0530

    Time: 6 ms (99.71%), Space: 46.4 MB (86.92%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,24 +1,58 @@
 class Solution {
+
+    class pair{
+        String s;
+        int f;
+        pair(String s, int f){
+            this.s = s;
+            this.f = f;
+        }
+    }
+    
     public List<String> topKFrequent(String[] words, int k) {
+
+        // HashMap<String, Integer> map = new HashMap<>();
+        // for(String i : words){
+        //     map.put(i, map.getOrDefault(i,0)+1);
+        // }
+
+        // PriorityQueue<String> pq = new PriorityQueue<>((a,b)->{
+        //     if(map.get(a).equals(map.get(b))) return a.compareTo(b);
+        //     return Integer.compare(map.get(b), map.get(a));
+        // });
+        
+        // for(String i : map.keySet()){
+        //     pq.offer(i);
+        // }
+        
+        // List<String> ls = new ArrayList<>();
+        // for(int i=0;i<k;i++){
+        //     ls.add(pq.poll());
+        // }
+        // return ls;
 
         HashMap<String, Integer> map = new HashMap<>();
         for(String i : words){
             map.put(i, map.getOrDefault(i,0)+1);
         }
 
-        PriorityQueue<String> pq = new PriorityQueue<>((a,b)->{
-            if(map.get(a).equals(map.get(b))) return a.compareTo(b);
-            return Integer.compare(map.get(b), map.get(a));
+        PriorityQueue<pair> pq = new PriorityQueue<>((a,b)->{
+            if(a.f != b.f){
+                return Integer.compare(b.f, a.f);
+            }
+            return (a.s).compareTo(b.s);
         });
-        
-        for(String i : map.keySet()){
-            pq.offer(i);
+
+        for(String s : map.keySet()){
+            pq.add(new pair(s, map.get(s)));
         }
-        
+
         List<String> ls = new ArrayList<>();
-        for(int i=0;i<k;i++){
-            ls.add(pq.poll());
+        for(int i = 0; i < k; i++){
+            ls.add(pq.poll().s);
         }
         return ls;
+
+
     }
 }
