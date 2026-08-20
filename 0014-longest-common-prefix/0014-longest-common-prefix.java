78b847f1e4c2d9ab17d3cf58fede1b9b66195bcf
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-20 17:26:31 +0530

    Time: 1 ms (61.08%), Space: 43.1 MB (67.95%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+        if(strs.length == 0) return "";
+        Arrays.sort(strs);
+        String a = strs[0];
+        String b = strs[strs.length - 1];
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < Math.min(a.length(), b.length()); i++){
+            if(a.charAt(i) == b.charAt(i)){
+                sb.append(a.charAt(i));
+            }
+            else break;
+        }
+        return sb.toString();
+    }
+}
