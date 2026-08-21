56ed118eeecf3d56fba6b623974e9b930db04a95
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-21 16:06:16 +0530

    Time: 0 ms (100%), Space: 46.6 MB (18.21%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+        int min = prices[0];
+        int max = 0;
+        for(int i = 1; i < prices.length; i++){
+           profit = prices[i] - min;
+           if(profit < 0){
+            min = prices[i];
+           }
+           else if(profit > 0){
+            max = max + profit;
+            min = prices[i];
+           }
+        }
+        return max;
+    }
+}
