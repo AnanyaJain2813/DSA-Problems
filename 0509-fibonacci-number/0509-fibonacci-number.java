53515b6ca528ef270dc07db856a173b3ca817c46
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-23 14:05:26 +0530

    Time: 10 ms (26.51%), Space: 41.8 MB (85.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,20 @@
 class Solution {
     public int fib(int n) {
 
-        int a = 0;
-        int b = 1;
-        if(n == 0) return 0;
-        else if(n == 1) return 1;
-        int next = 0;
-        for(int i = 2; i <= n; i++){
-            next = a + b;
-            a = b;
-            b = next;
-        }
-        return next;
+        // int a = 0;
+        // int b = 1;
+        // if(n == 0) return 0;
+        // else if(n == 1) return 1;
+        // int next = 0;
+        // for(int i = 2; i <= n; i++){
+        //     next = a + b;
+        //     a = b;
+        //     b = next;
+        // }
+        // return next;
+
+        if(n == 1)return 1;
+        if(n == 0)return 0;
+        return fib(n-1)+fib(n-2);
     }
 }
