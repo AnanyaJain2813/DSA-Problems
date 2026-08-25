356b4f6e1037a861bb182be1b55816b5b836e178
ANANYA JAIN <jainananya2800@gmail.com>
2026-08-25 16:32:56 +0530

    Time: 2 ms (68.2%), Space: 45.8 MB (6.83%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,12 +1,7 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
 
-        // int max = Integer.MIN_VALUE;
-        // for(int i = 0; i < nums.length; i++){
-        //     max = Math.max(nums[i], max);
-        // }
-
-        // for(int i = 1)
+     
 
         Set<Integer> st = new HashSet<>();
         for(int i : nums){
@@ -14,10 +9,8 @@ class Solution {
         }
 
         int aa = k;
-        int i = 1;
         while(st.contains(aa)){
-            i++;
-            aa = i*k;
+            aa = aa + k;
         }
         return aa;
     }
