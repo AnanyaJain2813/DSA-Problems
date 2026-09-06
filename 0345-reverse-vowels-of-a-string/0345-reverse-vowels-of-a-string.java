5ba79f98ff304dd99eb0bc865d589b8774983c47
ANANYA JAIN <jainananya2800@gmail.com>
2026-09-06 18:55:38 +0530

    Time: 2 ms (99.26%), Space: 46.6 MB (64.4%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -5,28 +5,53 @@ class Solution {
     }
     public String reverseVowels(String s) {
 
-       char ch[] = s.toCharArray();
+    //    char ch[] = s.toCharArray();
 
-       int i = 0, j = s.length() - 1;
+    //    int i = 0, j = s.length() - 1;
        
-       while(i < j){
-        char a = ch[i];
-        char b = ch[j];
+    //    while(i < j){
+    //     char a = ch[i];
+    //     char b = ch[j];
+    //     while(i < j && !vo(a)){
+    //         i++;
+    //         a = ch[i];
+    //     }
+    //     while(i < j && !vo(b)){
+    //         j--;
+    //         b = ch[j];
+    //     }
+
+    //     char te = ch[i];
+    //     ch[i] = ch[j];
+    //     ch[j] = te;
+    //     i++;
+    //     j--;
+    //    }       
+    //    return new String(ch);
+
+    char c[] = s.toCharArray();
+    int i = 0, j = c.length - 1;
+
+    while(i <= j){
+        char a = c[i];
+        char b = c[j];
+
         while(i < j && !vo(a)){
             i++;
-            a = ch[i];
+            a = c[i];
         }
         while(i < j && !vo(b)){
             j--;
-            b = ch[j];
+            b = c[j];
         }
-
-        char te = ch[i];
-        ch[i] = ch[j];
-        ch[j] = te;
-        i++;
-        j--;
-       }       
-       return new String(ch);
+      
+            char t = c[i];
+            c[i] = c[j];
+            c[j] = t;
+            i++;
+            j--;
+        
+    }
+    return new String(c);
     }
 }
