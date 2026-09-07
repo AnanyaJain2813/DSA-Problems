24a05b1f528962290c9a06a0625f1e21eac3f7ac
ANANYA JAIN <jainananya2800@gmail.com>
2026-09-07 15:08:54 +0530

    Time: 0 ms (100%), Space: 44 MB (28.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,16 +6,16 @@ class Solution {
             int mid = (low + high)/2;
             if(nums[mid] == target) return mid;
             if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid]){
-                    high = mid;
+                if(nums[low] <= target && target < nums[mid]){
+                    high = mid - 1;
                 }
                 else{
                     low = mid + 1;
                 }
             }
             else{
-                    if(nums[mid] <= target && target <= nums[high]){
-                        low = mid;
+                    if(nums[mid] < target && target <= nums[high]){
+                        low = mid + 1;
                     }
                     else high = mid - 1;
             }
