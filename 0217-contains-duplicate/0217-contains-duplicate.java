85e177ff9e522a3eb2e01e900a0b67d0b766e758
ANANYA JAIN <jainananya2800@gmail.com>
2026-09-08 09:04:52 +0530

    Time: 43 ms (5.1%), Space: 110.3 MB (10.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
-       HashSet<Integer> st = new HashSet<>();
-       for(int i = 0; i < nums.length; i++){
-        if(st.contains(nums[i])) return true;
-        st.add(nums[i]);
-       }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        for(int i : map.keySet()){
+            if(map.get(i) >= 2) return true;
+        }
         return false;
     }
 }
