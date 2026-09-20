701bb10005309d5df59ce25a1585f183f86a0af6
ANANYA JAIN <jainananya2800@gmail.com>
2026-09-20 23:42:00 +0530

    Time: 2 ms (45.72%), Space: 94.8 MB (15.74%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,30 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
-    //     int max_profit = 0;
-    //     int n = prices.length;
-    //     int buy = prices[0];
-    //     for(int i = 1; i < n; i++){
-    //          int profit = prices[i] - buy;
-    //          max_profit = Math.max(profit, max_profit);
-    //          buy = Math.min(buy, prices[i]);
-    //     }
+        // int m_profit = 0;
+        // for(int i = 0; i < prices.length; i++){
+        //     int profit = 0;
+        //     int buy = prices[i];
+        //     for(int j = i + 1; j < prices.length; j++){
+        //         profit = prices[j] - buy;
+        //         m_profit = Math.max(profit, m_profit);
+        //     }
+        // } 
+        // return m_profit;
 
-        
-    //     return max_profit;
-
-     int max = 0;
-     int min = prices[0];
-     int profit = 0;
-     for(int i = 1; i < prices.length; i++){
-        profit = prices[i] - min;
-        if(profit < 0){
-            min = prices[i];
+        int max = 0;
+        int buy = prices[0];
+        for(int i = 1; i < prices.length; i++){
+            int p = prices[i] - buy;
+            buy = Math.min(buy, prices[i]);
+            max = Math.max(p, max);
         }
-        else{
-            max = Math.max(max, profit);
-        }
-     }
-     return max;
+        return max;
     }
 }
